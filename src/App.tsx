a9088c345bcfe641fa71eb91a15a69066e936019
Estheraiyeola <estheraiyeola@yahoo.com>
2024-05-31 08:29:21 +0100

    Organized the project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useRoutes } from 'react-router-dom';
+import {ROUTES} from './routes'
+
+function App() {
+  
+  return (
+    useRoutes(ROUTES)
+  );
+}
+
+export default App;
---
+++ src/secret.tsx
@@ -0,0 +1,3 @@
+export const googleAuthCredentials = {
+    CLIENT_ID :'653728099437-78ikm3phiglctpicjh7bb0qp7vli6548.apps.googleusercontent.com'
+} 
